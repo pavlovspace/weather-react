7bd001b3dacadc52ed5afb5c39bcb407dbb4fccd
Paul Pavlov <pavlov1space@gmail.com>
2025-02-20 16:24:27 +0100

    fix: update base path for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/weather-app/', 
+  base: '/weather-react/', 
   plugins: [react()],
 })
